d027ad477d98a2cb14bcf03ce84432034509c4d1
Ishaan300104 <ishaan300104@gmail.com>
2025-10-27 17:17:55 +0530

    fixed path issue, increased lr

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 3, 
-        "lr": 10 ** -4,
+        "num_epochs": 5, 
+        "lr": 3e-4,
         "seq_len": 350,
         "d_model": 512,
         "lang_src": "en",
@@ -18,6 +18,6 @@ def get_config():
 
 def get_weights_file_path(config, epoch: str):
     model_folder = config['model_folder']
-    model_basename = config['model_basename']
+    model_basename = config['model_filename']
     model_filename = f"{model_basename}{epoch}.pt"
     return str(Path('.') / model_folder / model_filename)
